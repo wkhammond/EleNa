2523789e46b41c1a301f2eb19619ce088a912ad1
Th3OnlyN00b <pathos.smith@gmail.com>
2020-04-15 16:05:25 -0400

    converting to react

--- elena1/src/App.js
+++ elena1/src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 import './index.css';
 import './maps/map.js';
 import './maps/markers.js';
-import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import QueryForm from './components/Query'
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 
 
 function App() {
@@ -14,24 +15,7 @@ function App() {
 
             <p>Input your starting and ending locations, and answer the additional questions to get a route. </p>
 
-            <div id="map" style={{ height: 440, border: 1 }}></div>
-
-            <form class="form-inline" action="/action_page.php">
-                <div class="form-group">
-                    <label for="start">Starting Location:</label>
-                    <input type="start" class="form-control" id="start"></input>
-                </div>
-                <div class="form-group">
-                    <label for="end">Ending Location:</label>
-                    <input type="end" class="form-control" id="end"></input>
-                </div>
-                <div class="slidecontainer">
-                    <label for="elevation">How important are changes in elevation?</label>
-                    <input type="range" min="1" max="100" value="50" class="slider" id="elevation"></input>
-                    <div id="demo"></div>
-                </div>
-                <button type="submit" class="btn btn-default">Submit</button>
-            </form>
+            <QueryForm/>
         </div>
     );
 }
---
+++ elena1/src/components/Query/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import {
+    Card,
+    CardContent,
+    CardHeader,
+    Button,
+    TextField,
+} from '@material-ui/core';
+
+const styles = theme => ({
+    card: {
+        minWidth: 200,
+    },
+    button: {
+        margin: theme.spacing(1),
+    },
+})
+
+class QueryForm extends React.Component {
+    constructor(props) {
+        super(props); // This line is always required to be the first line
+        this.state = {
+        }
+    }
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <Card>
+                <form>
+                    <TextField
+                        id="test-field"
+                        label="TEST LABEL"
+                        variant="outlined"
+                    />
+                </form>
+            </Card>
+        );
+    }
+}
+
+export default withStyles(styles)(QueryForm); //This is a function which allows the const styles to be applied to the component
