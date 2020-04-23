d18fa6dcc755a583a640bbb173d2b3faa19169fc
Alexander Vukovic <alexvukovic96@gmail.com>
2020-04-23 12:52:57 -0400

    depreciated generate_model.py for now

--- backend/generate_model.py
+++ backend/generate_model.py
@@ -3,17 +3,4 @@ import networkx as nx
 import numpy as np
 import requests as rq
 
-G = ox.graph_from_place('Boston, Masssachusetts', network_type='drive')
-ox.save_graphml(G, "boston.graphml")
-ox.plot_graph(G, show=False, save=True, filename='network',file_format='svg')  
-print("done")
-
-
-def make_model():
-    #generates and saves our model
-    return 1
-
-def get_elevation(lat, long):
-    #pings USGS and returns the elevation at a specific point
-    return 1
-    
+#this file is currently depreciated    
--- backend/model.py
+++ backend/model.py
@@ -9,7 +9,33 @@ ox.plot_graph(G, show=False, save=True, filename='network',file_format='svg')
 print("done")
 
 
-def make_model():
-    #generates and saves our model
+def get_path(origin, destination, weight):
+    """Returns a path 
+
+    Parameters
+    ----------
+    origin : string
+       String pertaining to origin's address
+
+    destination: string
+       String pertaining to destination's address
+
+    weight: int
+       value that pertains to how much we want to weigh elevation
+
+
+    Returns
+    -------
+    graph: NetworkX graph
+        Returns a 
+
+    Notes
+    -----
+    Current implementation doesn't involve caching- so the model file will 
+    have to be generated every time we run this. Future versions will involve 
+    caching the graph/map and loading it from disk.
+        
+    """
+    
     return 1
     
